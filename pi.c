5f6e72e513b57a540979028b214ed36dce61b179
a boy <a-boy@users.noreply.github.com>
2025-04-21 14:11:33 +0800

    Add files via upload

---
+++ pi.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+long a=10000,b=0,c=28000,d,e=0,f[28001],g;
+int main(){
+	for(;b-c;)
+		f[b++]=a/5;
+	for(;d=0,g=c*2;c-=14,printf("%.4d",e+d/a),e=d%a)
+		for(b=c;d+=f[b]*a,f[b]=d%--g,d/=g--,--b;d*=b);
+	return 0;
+}
